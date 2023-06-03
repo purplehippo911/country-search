e715876e18cc8332625418173fcd08d0eda0cf9c
purplehippo911 <76628594+purplehippo911@users.noreply.github.com>
2023-06-03 22:51:21 +0200

    Fixed code because of production problems

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import useStore from '../store/store';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Search from '@comps/search'
 import Card from '@comps/Card'
@@ -25,7 +24,7 @@ export default function Home({countries}) {
   useEffect(() => {
     setSearchedCountries(countriesArray)
     setIsLoading(false)
-  }, [countries]);
+  });
 
   
   return (
