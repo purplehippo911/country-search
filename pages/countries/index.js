c436a741e68e16bdf9a9d43a92449f9c7abf6c20
purplehippo911 <76628594+purplehippo911@users.noreply.github.com>
2023-06-03 23:56:45 +0200

    Fixed image indexing

--- pages/countries/index.js
+++ pages/countries/index.js
@@ -5,7 +5,7 @@ export default function Countries() {
             <Link href="/" className="btn justify-self-start flex items-center w-1/2">
                 <picture className="w-10">
                     <img 
-                        src="../left-arrow.png" 
+                        src="/left-arrow.png" 
                         className="scale-[0.5] w-full"
                         alt="arrow pointing to the left to indicate how to go back to home"
                     />
