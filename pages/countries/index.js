58263881cb2a55ff7dd8fc3b4d5e769ded23c80f
purplehippo911 <76628594+purplehippo911@users.noreply.github.com>
2023-06-03 23:47:03 +0200

    Update index.js

--- pages/countries/index.js
+++ pages/countries/index.js
@@ -1,14 +1,14 @@
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Countries() {
     return ( 
             <Link href="/" className="btn justify-self-start flex items-center w-1/2">
                 <picture className="w-10">
-                    <Image 
+                    <img 
                         src="../left-arrow.png" 
-                        className="scale-[0.5] w-full"/>
+                        className="scale-[0.5] w-full"
                         alt="arrow pointing to the left to indicate how to go back to home"
+                    />
                 </picture>
                 <button>Back to search for countries</button>
             </Link>
